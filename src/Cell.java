0eb9076091198a5605e519da4b66afda3be332ad
Laura Paganini <Laura Paganini>
2017-04-24 23:01:00 -0400

    starting temperature sim

--- src/Cell.java
+++ src/Cell.java
@@ -6,7 +6,7 @@ public class Cell {
 	private double rLen, lLen, bLen, tLen;
 	private double sunAngle;
 
-	private Cell north, south, east, west;
+	public Cell north, south, east, west;
 
 	public Cell(double temp, int tLat, int bLat, int rLon, int lLon, double sideLen) {
 		this.temp = temp;
@@ -40,31 +40,18 @@ public class Cell {
 	}
 
 	public void calculateArea() {
-		
 		double height;
-		
-		//if (bLat >= 0) {
-			if (tLat == 90)
-				tLen = 0;
-			else
-				tLen = this.north.bLen; // the length of the top of this cell is
-										// the length of the bottom on the north
-										// cell
-			bLen = (Math.cos(Math.toRadians(bLat)) * rLen);
-			height = Math.sqrt(Math.pow(this.rLen, 2) - Math.pow(this.bLen - this.tLen, 2)/4.0);
-			area = ((tLen + bLen) / 2) * height;
-//		} else {
-//			if (bLat == -90)
-//				bLen = 0;
-//			else
-//				bLen = this.south.tLen; // the length of the bottom of this cell
-//										// is the length of the top on the south
-//										// cell
-//			tLen = (Math.cos(Math.toRadians(tLat)) * lLen);
-//			height = Math.sqrt(Math.pow(this.rLen, 2) - Math.pow(this.bLen - this.tLen, 2));
-//			
-//			area = ((tLen + bLen) / 2) * height;
-//		}
+
+		if (tLat == 90)
+			tLen = 0;
+		else
+			tLen = this.north.bLen; // the length of the top of this cell is
+									// the length of the bottom on the north
+									// cell
+		bLen = (Math.cos(Math.toRadians(bLat)) * rLen);
+		height = Math.sqrt(Math.pow(this.rLen, 2) - Math.pow(this.bLen - this.tLen, 2) / 4.0);
+		area = ((tLen + bLen) / 2) * height;
+
 	}
 
 }
--- src/Earth.java
+++ src/Earth.java
@@ -4,46 +4,61 @@ public class Earth {
 	private static int DEFAULT_SPACING = 15, EARTH_CIRCUMFERENCE = 40075000;
 	private static double INITIAL_TEMP = 15;
 	private Cell[][] globe;
-	
-	public Earth(int spacing){
+
+	public Earth(int spacing, int timeInterval) {
 		this.d = 180 / spacing;
 		initializeGlobe();
+		updateSim(timeInvterval);
 	}
-	
-	public Earth(){
+
+	public Earth() {
 		new Earth(DEFAULT_SPACING);
 	}
-	
-	private void initializeGlobe(){
-		globe = new Cell[d][d];
-		int tLat = 90;
-		
-		for(int i = 0; i < d; i++){
-			for(int j = 0; j < d; j++){
-	
-				globe[i][j] = new Cell(INITIAL_TEMP, tLat, tLat - 180/d, j*(180/d), (j*(180/d)+(180/d)), (double)(EARTH_CIRCUMFERENCE)/(2*d));	//check north and west of globe[0][0]
-				
-			}
 
-			tLat -= 180/d;
-			
-			
+	private void initializeGlobe() {
+		int tLat = 90;
+
+		for (int i = 0; i < d; i++) {
+			for (int j = 0; j < d; j++) {
+
+				globe[i][j] = new Cell(INITIAL_TEMP, tLat, tLat - 180 / d, j * (180 / d), (j * (180 / d) + (180 / d)),
+						(double) (EARTH_CIRCUMFERENCE) / (2 * d)); 
+
+			}
+			tLat -= 180 / d;
 		}
-		
-		for(int i = 0; i < d; i++){
-			for(int j = 0; j < d; j++){
-		
-				globe[i][j].setNeighbors(globe[((i - 1)%d > 0)?(i - 1)%d : i + (d/2)%d][j], globe[(i + 1)%d][j], globe[i][((j - 1)%d > 0)? (j - 1)%d : d-1], globe[i][(j+1)%d]);
+
+		for (int i = 0; i < d; i++) {
+			for (int j = 0; j < d; j++) {
+
+				globe[i][j].setNeighbors(globe[((i - 1) % d > 0) ? (i - 1) % d : i + (d / 2) % d][j],
+						globe[(i + 1) % d][j], globe[i][((j - 1) % d > 0) ? (j - 1) % d : d - 1],
+						globe[i][(j + 1) % d]);// check north and west of globe[0][0]
 				globe[i][j].calculateArea();
-				//System.out.printf("%3d ", globe[i][j].tLat);
+				// System.out.printf("%3d ", globe[i][j].tLat);
+			}
+			//System.out.println();
+		}
+	}
+	
+	
+	private void updateSim(int timeInterval){
+		double cellAverages, sunTemp;
+		Cell thisCell;
+		
+		
+		for (int i = 0; i < d; i++) {
+			for (int j = 0; j < d; j++) {
+				thisCell = globe[i][j];
+				cellAverages = (thisCell.getTemp() + thisCell.north.getTemp() + thisCell.south.getTemp() + thisCell.west.getTemp() + thisCell.east.getTemp())/5;
+				//sunTemp = 
+				
 				
 				
 			}
-			
-			System.out.println();
 		}
 		
-		
+		//increment sunPos
 		
 	}
 }
