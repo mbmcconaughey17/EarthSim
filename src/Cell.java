eb9e9e4a0fb275eaea6a4dbf5afa99691acdb61c
Laura Paganini <Laura Paganini>
2017-04-24 21:57:27 -0400

    area!

--- src/Cell.java
+++ src/Cell.java
@@ -1,55 +1,70 @@
 
 public class Cell {
 	private double temp;
-	double area;
+	public double area;
 	public int rLon, lLon, bLat, tLat;
 	private double rLen, lLen, bLen, tLen;
 	private double sunAngle;
-	
+
 	private Cell north, south, east, west;
-	
-	public Cell(double temp, int tLat, int bLat, int rLon, int lLon, double sideLen){
+
+	public Cell(double temp, int tLat, int bLat, int rLon, int lLon, double sideLen) {
 		this.temp = temp;
-		
-		
-		
+
 		this.bLat = bLat;
 		this.tLat = tLat;
 		this.lLon = lLon;
 		this.rLon = rLon;
-		
+
 		rLen = sideLen;
 		lLen = sideLen;
 	}
-	
-	public void setSunAngle(double sunAngle){
+
+	public void setSunAngle(double sunAngle) {
 		this.sunAngle = sunAngle;
 	}
-	
-	public void setTemp(double temp){
+
+	public void setTemp(double temp) {
 		this.temp = temp;
 	}
-	
-	public double getTemp(){
+
+	public double getTemp() {
 		return temp;
 	}
-	
-	public void setNeighbors(Cell north, Cell south, Cell west, Cell east){
+
+	public void setNeighbors(Cell north, Cell south, Cell west, Cell east) {
 		this.north = north;
 		this.south = south;
 		this.east = east;
 		this.west = west;
 	}
-	
-	public void calculateArea(){
-		if(tLat == 90)
-			tLen = 0;
-		else
-			tLen = this.north.bLen;			//the length of the top of this cell is the length of the bottom on the north cell
-		bLen = (Math.cos(Math.toRadians(bLat))*rLen) * 2 + tLen;
+
+	public void calculateArea() {
 		
-		area = ((tLen + bLen)/ 2)* Math.sin(Math.toRadians(bLat));
+		double height;
+		
+		//if (bLat >= 0) {
+			if (tLat == 90)
+				tLen = 0;
+			else
+				tLen = this.north.bLen; // the length of the top of this cell is
+										// the length of the bottom on the north
+										// cell
+			bLen = (Math.cos(Math.toRadians(bLat)) * rLen);
+			height = Math.sqrt(Math.pow(this.rLen, 2) - Math.pow(this.bLen - this.tLen, 2)/4.0);
+			area = ((tLen + bLen) / 2) * height;
+//		} else {
+//			if (bLat == -90)
+//				bLen = 0;
+//			else
+//				bLen = this.south.tLen; // the length of the bottom of this cell
+//										// is the length of the top on the south
+//										// cell
+//			tLen = (Math.cos(Math.toRadians(tLat)) * lLen);
+//			height = Math.sqrt(Math.pow(this.rLen, 2) - Math.pow(this.bLen - this.tLen, 2));
+//			
+//			area = ((tLen + bLen) / 2) * height;
+//		}
 	}
-	
-	
+
 }
--- src/Earth.java
+++ src/Earth.java
@@ -16,24 +16,17 @@ public class Earth {
 	
 	private void initializeGlobe(){
 		globe = new Cell[d][d];
-		int tLat = 90, bLat = 90 -(180/d);
+		int tLat = 90;
 		
 		for(int i = 0; i < d; i++){
 			for(int j = 0; j < d; j++){
 	
-				globe[i][j] = new Cell(INITIAL_TEMP, tLat, bLat, j*(180/d), (j*(180/d)+(180/d)), (double)(EARTH_CIRCUMFERENCE)/(2*d));	//check north and west of globe[0][0]
+				globe[i][j] = new Cell(INITIAL_TEMP, tLat, tLat - 180/d, j*(180/d), (j*(180/d)+(180/d)), (double)(EARTH_CIRCUMFERENCE)/(2*d));	//check north and west of globe[0][0]
 				
 			}
 
-			if(i < d/2){
-				tLat -= 180/d;
-				bLat -= 180/d;
-			}
+			tLat -= 180/d;
 			
-			else{
-				tLat += 180/d;
-				bLat += 180/d;
-			}
 			
 		}
 		
@@ -42,7 +35,7 @@ public class Earth {
 		
 				globe[i][j].setNeighbors(globe[((i - 1)%d > 0)?(i - 1)%d : i + (d/2)%d][j], globe[(i + 1)%d][j], globe[i][((j - 1)%d > 0)? (j - 1)%d : d-1], globe[i][(j+1)%d]);
 				globe[i][j].calculateArea();
-				System.out.printf("%3f ", globe[i][j].area);
+				//System.out.printf("%3d ", globe[i][j].tLat);
 				
 				
 			}
